d767a4ad81adb64867036ed44c8196fc77cd617c
SalmaMeniawy <salmamostafameniawy@gmail.com>
2019-10-06 13:43:16 +0200

    create the constructor of the DB class that take parameters for connection

--- utility/db.php
+++ utility/db.php
@@ -3,5 +3,9 @@
     class DB{
         private static $connection = NULL;
 
+        private function __construct($db_host ,$db_user,$db_pswd ,$db_name)
+        {
+            
+        }
     }
 ?>
