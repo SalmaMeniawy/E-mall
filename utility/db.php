50bb3fecce22e02a3d27d23ef6a0d32ead91e967
Mohammed-Elmasry <38670846+Mohammed-Elmasry@users.noreply.github.com>
2019-10-06 14:39:21 +0200

    add condition to check for object existence

--- utility/db.php
+++ utility/db.php
@@ -9,7 +9,11 @@
          */
         private function __construct($db_host ,$db_user,$db_pswd ,$db_name)
         {
-            $this->connection = new mysqli($db_host,$db_user,$db_pswd,$db_name);
+            // first check if there's an object already loaded in memory..
+            if($this->connection == NULL){
+                $this->connection = new mysqli($db_host,$db_user,$db_pswd,$db_name);
+            }
+            //return the existing object anyway (newly created or created before this function call).
             return $this->connection;
         }
 
