ba1fe4c2a4a9dbdd5a563783bdbe94aa183f381a
SalmaMeniawy <salmamostafameniawy@gmail.com>
2019-10-06 13:44:56 +0200

    create function set_connection to start the connection to DB which take parameters host name , user , pswd and finally the DB name that will be use in the connection

--- utility/db.php
+++ utility/db.php
@@ -5,6 +5,15 @@
 
         private function __construct($db_host ,$db_user,$db_pswd ,$db_name)
         {
+
+        }
+        public static function set_connection($host,$user,$pswd,$name)
+        {   
+            $db_host = $host;
+            $db_user = $user;
+            $db_pswd = $pswd;
+            $db_name = $name;
+            $this->connection = new mysqli($db_host,$db_user,$db_pswd,$db_name);
             
         }
     }
