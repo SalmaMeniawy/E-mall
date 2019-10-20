11572d16d5573a5d3f35d06f63b6495baf99cb3b
Mohammed-Elmasry <mohammed.hussein.business@gmail.com>
2019-10-20 17:11:23 +0200

    create global_loader.php

---
+++ global_loader.php
@@ -0,0 +1,2 @@
+<?php
+    require_once __DIR__ . "/resources/template_engine.php";
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-    require_once __DIR__ . "/vendor/autoload.php";
+    // require_once __DIR__ . "/vendor/autoload.php";
     use smarty\templateEngine\TemplateEngine;
 
     $smarty = TemplateEngine::get_smarty();
