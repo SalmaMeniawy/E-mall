395bd53031913863cdadc62df2653557f3499cb9
Mohammed-Elmasry <mohammed.hussein.business@gmail.com>
2019-11-07 06:50:03 +0200

    define single point of access to application

--- index.php
+++ index.php
@@ -1,15 +1,10 @@
 <?php
-    require_once __DIR__ . "/global_loader.php";
-    use smarty\templateEngine\TemplateEngine;
-    use function helpers\config;
+require_once __DIR__ . "/global_loader.php";
 
-    $smarty = TemplateEngine::get_smarty();
-    
-    $array = ['thor','loki','odin'];
-    $smarty->assign("gods", $array);
-    
-    $smarty->template_dir = __DIR__ . config("TemplateDir");
-    $smarty->compile_dir  = __DIR__ . config("cmpTemplateDir");
-    $smarty->display("homepage.tpl.php");
+use smarty\templateEngine\TemplateEngine;
+use function helpers\config;
 
-
+$smarty = TemplateEngine::get_smarty();
+$array = ['thor', 'loki', 'odin'];
+$smarty->assign("gods", $array);
+$smarty->display("index.tpl.php");
