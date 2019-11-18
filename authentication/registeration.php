2f10d46d31bca72d12b6e77cae0b2c8e2bc860c7
SalmaMeniawy <salmamostafameniawy@gmail.com>
2019-11-18 11:02:49 +0200

    create function username_available_before and it work return object if it available and add form for test

--- authentication/registeration.php
+++ authentication/registeration.php
@@ -10,13 +10,13 @@ class Registration{
     public function  __set($proparty ,$value){
         $this->data[$proparty] = $value;
     }
-    public function __construct(String $username,String $email,$password,string $dob,String $country)
+    public function __construct(String $Name,String $Email,$Password,string $Dob,String $Country)
     {
-        $this->__set($username,$username);
-        $this->__set($email,$email);
-        $this->__set($password,$password);
-        $this->__set($dob,$dob);
-        $this->__set($country,$country);
+        $this->__set('username',$Name);
+        $this->__set('email',$Email);
+        $this->__set('password',$Password);
+        $this->__set('dob',$Dob);
+        $this->__set('country',$Country);
         
     }
     
@@ -28,19 +28,57 @@ class Registration{
         return $var_name;
     }
     public function username_available_before(){
-       $database = DB::create_connection(config('dbhost'),config('dbuser'),config('dbpass'),config('dbname'));
-        $username =$this-> __get('username');
-       $result = $database->read_all_records('users');
-       foreach($result as $var){
-           var_dump($var);
-       }
-    
+         $database = DB::create_connection(config('dbhost'),config('dbuser'),config('dbpass'),config('dbname'));
+         $username =$this-> __get('username');
+         echo $username;
+         $result = $database->read('users',['name'],["'name'= '.$username.'"]);
+       return $result;
     }
 }
 
 // <?php $_SERVER['PHP_SELF']
 ?>
+<!DOCTYPE html>
+<html>
+    <head>
+
+    </head>
+    <body>
+        <form action="registeration.php" method="POST">
+            <div>
+                <input type="text" name="username" placeholder="User Name" require>
+            </div>
+            <div>
+                <input type="email" name="email"placeholder="Email" require >
+            </div>
+            <div>
+                <input type="password" name="password" placeholder="password" require>
+            </div>
+            <div>
+                <input type="date" name="dob" placeholder="Date Of Birth" require>
+            </div>
+            <div>
+                <input type="text" name="country" placeholder="Country" require>
+            </div>
+            <div>
+                <button type="submit" >Sign Up</button>
+            </div>
+        </form>
+    </body>
 <?php
+echo "Hello hiiiiiiiiiiiii";
+   if(isset($_POST['username']) && !is_null($_POST['username'])){
+    echo "Hello hiiiiiiiiiiiii";
+    echo "<br>";
+
+    $regist = new Registration($_POST['username'],$_POST['email'],$_POST['password'],$_POST['dob'],$_POST['country']);
+    $result =$regist->username_available_before();
+    var_dump($result);
+    if($result){
+        echo "<br>";
+        echo "it is available before";
+    }
+   }
     // $database = DB::create_connection(config('dbhost'),config('dbuser'),config('dbpass'),config('dbname'));
     // $result = $database->read_fields('users',['name']);
     // $result1 = $database->read('users',['name'],["'name'= 'salma'"]);
